282feeca62a8d71215c963d72b6a8cc7345b2cee
madhvibansal <madhvi.mitrc@gmail.com>
2022-01-18 20:38:22 +0530

    change develop

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -8,9 +8,9 @@ const ExpenseItem = (props) => {
   return (
     <li>
       <Card className='expense-item'>
-        <ExpenseDate date={props.date} />
+        <ExpenseDate date={props.title} />
         <div className='expense-item__description'>
-          <h2>{props.title}</h2>
+          <h2>{props.date}</h2>
           <div className='expense-item__price'>${props.amount}</div>
         </div>
       </Card>
